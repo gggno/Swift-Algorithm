a8b109b22d8535633212bd58c5b2f750663d1a07
정근호 <geunhojeong.jgh@gmail.com>
2022-10-29 15:52:13 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	cin >> num1;
+	cin >> num2;
+
+	if (num1 > num2)
+		cout << ">";
+
+	else if (num1 < num2)
+		cout << "<";
+
+	else
+		cout << "==";
+
+	return 0;
+}
