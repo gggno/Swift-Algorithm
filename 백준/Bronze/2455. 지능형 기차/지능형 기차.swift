25335450fa1ed79c4bc4e00aeb41b6ed8ea63741
정근호 <geunhojeong.jgh@gmail.com>
2022-11-09 16:58:21 +0900

    [Bronze III] Title: 지능형 기차, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+var numberPeopleArr: [Int] = []
+numberPeopleArr.append(0)
+for i in 0...3 {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    
+    numberPeopleArr.append(numberPeopleArr[i] + (input[1] - input[0]))
+}
+print(numberPeopleArr.max()!)
