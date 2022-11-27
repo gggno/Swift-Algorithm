65a68e3f6975a32bd254b22bc866f928426782cb
정근호 <geunhojeong.jgh@gmail.com>
2022-11-27 18:27:51 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.swift
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.swift
@@ -3,23 +3,20 @@ import Foundation
 let input = Int(readLine()!)!
 var res = Array(readLine()!)
 
-var resultArr: [String] = []
-
-if input > 1 {
-    for i in 0...input-2 {
+if input > 1 { // 개수가 1 이상일때
+    for _ in 0...input-2 {
         let str = Array(readLine()!)
+        
         for j in 0...str.count-1 {
-            if res[j] == str[j] {
+            if res[j] == str[j] { // 문자가 같을 때
                 continue
-            } else if res[j] != str[j] {
+                
+            } else if res[j] != str[j] { // 문자가 다를 때
                 res[j] = "?"
             }
         }
     }
-    resultArr.append(String(res))
-    print(resultArr[0])
-} else {
-    resultArr.append(String(res))
-    print(resultArr[0])
+    print(String(res))
+} else { // 개수가 1 이하일때
+    print(String(res))
 }
-
