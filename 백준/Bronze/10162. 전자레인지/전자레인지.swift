fa6fad1f8e9d3a24159c0aeca4a7c436f9a6adb0
정근호 <geunhojeong.jgh@gmail.com>
2023-01-22 21:58:24 +0900

    [Bronze IV] Title: 전자레인지, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var num = n
+var a = 0
+var b = 0
+var c = 0
+
+while num > 0 {
+    if (num / 500) > 0 {
+        a += num / 500
+        num -= (500 * (num / 500))
+        continue
+    } else if (num / 60) > 0 {
+        b += num / 60
+        num -= (60 * (num / 60))
+        continue
+    } else if (num / 10) > 0 {
+        c += num / 10
+        num -= (10 * (num / 10))
+        continue
+    } else {
+        break
+    }
+}
+
+if num == 0 {
+    print(a, b, c)
+} else {
+    print(-1)
+}
