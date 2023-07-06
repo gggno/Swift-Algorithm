cbc87852e6fc62c3f9a46ed231cd7c280318cf89
정근호 <geunhojeong.jgh@gmail.com>
2023-07-06 21:42:19 +0900

    [level 1] Title: 실패율, Time: 0.12 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var failDic: [Int:Double] = [:]
+    var arrCnt = stages.count
+
+    for i in 1...N {
+        var failCnt = 0
+        for j in 0..<stages.count {
+            if stages[j] == i {
+                failCnt += 1
+            }
+        }
+        failDic[i] = Double(failCnt) / Double(arrCnt)
+        arrCnt -= failCnt
+    }
+    let result = failDic.sorted(by: <).sorted{ $0.1 > $1.1 }    
+    let last = result.map {$0.key}
+
+    return last
+}
