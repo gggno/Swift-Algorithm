d5bf1e402327ee8a795876519fe71f540e1b1f84
정근호 <geunhojeong.jgh@gmail.com>
2023-08-08 15:59:02 +0900

    [Silver III] Title: N과 M (6), Time: 12 ms, Memory: 79520 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input[0], m = input[1]
+let arr = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
+
+var stack: [Int] = []
+var answer = ""
+
+func dfs() {
+    if stack.count == m {
+        answer += stack.map{String($0)}.joined(separator: " ")
+        answer += "\n"
+        return
+    }
+    
+    for i in arr {
+        if !stack.contains(i) && stack.filter{$0 > i}.count == 0 {
+            stack.append(i)
+            dfs()
+            stack.removeLast()
+        }
+    }
+}
+
+dfs()
+print(answer)
