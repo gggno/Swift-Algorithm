51e5c009875ba2a36a71748cbd95e0d7fe674b69
정근호 <geunhojeong.jgh@gmail.com>
2023-08-09 16:49:15 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 80288 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
@@ -1,55 +1,46 @@
 import Foundation
 
 let input = readLine()!.split(separator: " ").map{Int($0)!}
-let n = input[0]    // 정점의 개수
-let m = input[1]    // 간선의 개수
-let v = input[2]    // 탐색을 시작할 정점의 번호
+let n = input[0], m = input[1], v = input[2]
 
-var graph: [Int:[Int]] = [:]
-var visited: Set<Int> = []
-var stack: [Int] = [v]
+var graph: [[Int]] = Array(repeating: [], count: n+1)
+var visited: [Bool] = Array(repeating: false, count: n+1)
+var visited2: [Bool] = Array(repeating: false, count: n+1)
+
 var result1: [Int] = []
-
-var queue: [Int] = [v]
 var result2: [Int] = []
 
-for i in 1...n {
-    graph[i] = []
+for _ in 0..<m {
+    let nums = readLine()!.split(separator: " ").map{Int($0)!}
+    graph[nums[0]].append(nums[1])
+    graph[nums[1]].append(nums[0])
+    graph[nums[0]].sort()
+    graph[nums[1]].sort()
 }
 
-for i in 0..<m {
-    let num = readLine()!.split(separator: " ").map{Int($0)!}
-    graph[num[0]]?.append(num[1])
-    graph[num[1]]?.append(num[0])
-}
-
-while !stack.isEmpty { // DFS
-    let vertex = stack.removeLast()
-    
-    if !visited.contains(vertex) {
-        visited.insert(vertex)
-        stack += graph[vertex]!.sorted(by: >)
-        result1.append(vertex)
-    }
-    
-}
-for i in result1 {
-    print(i, terminator: " ")
-}
-print("")
-visited.removeAll()
-
-while !queue.isEmpty {
-    let vertex = queue.removeFirst()
-    
-    if !visited.contains(vertex) {
-        visited.insert(vertex)
-        result2.append(vertex)
-        queue += graph[vertex]!.sorted(by: <)
+func dfs(_ v: Int) {
+    if !visited[v] {
+        visited[v] = true
+        result1.append(v)
+        for i in graph[v] {
+            dfs(i)
+        }
     }
 }
 
-for i in result2 {
-    print(i, terminator: " ")
+var queue: [Int] = [v]
+func bfs(_ v: Int) {
+    while !queue.isEmpty {
+        let pop = queue.removeFirst()
+        if !visited2[pop] {
+            visited2[pop] = true
+            result2.append(pop)
+            queue += graph[pop]
+        }
+    }
+    print(result2.map{String($0)}.joined(separator: " "))
 }
-print("")
+
+dfs(v)
+print(result1.map{String($0)}.joined(separator: " "))
+bfs(v)
