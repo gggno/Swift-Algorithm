097f6343cad8d259b36767b7f387ebd744c1d4c6
정근호 <geunhojeong.jgh@gmail.com>
2023-08-18 17:37:25 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.swift
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.swift
@@ -1,67 +1,46 @@
 import Foundation
 
-// bfs를 위한 Queue 구현
-struct Queue<T> {
-    var queue = [T]()
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input[0]
+let m = input[1]
+
+var graph: [[Bool]] = Array(repeating: Array(repeating: false, count: n+1), count: n+1)
+
+for _ in 0..<m {
+    let edge = readLine()!.split(separator: " ").map{Int($0)!}
+    graph[edge[0]][edge[1]] = true
+    graph[edge[1]][edge[0]] = true
+}
+
+func bfs(_ num: Int) -> Int {
+    var queue: [(Int, Int)] = [(num, 0)]
+    var visited: [Bool] = Array(repeating: false, count: n+1)
+    var cnt = 0
+
+    visited[num] = true
+
     var index = 0
+    while index < queue.count {
+        let now = queue[index]
+        cnt += now.1
 
-    var isEmpty: Bool {
-        return queue.count - index == 0
-    }
-
-    mutating func push(_ e: T) {
-        queue.append(e)
-    }
-
-    mutating func pop() -> T {
-        defer {
-            index += 1
-        }
-        return queue[index]
-    }
-}
-
-// 입력 받기
-let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-let N = input[0], M = input[1]
-
-// 인접 행렬 선언하고 간선 정보 저장하기
-    //🤔 이 문제는 인접리스트가 더 적절함!
-var matrix = Array(repeating: Array(repeating: false, count: N + 1), count: N + 1)
-for _ in 0..<M {
-    let edge = readLine()!.split(separator: " ").map { Int(String($0))! }
-    matrix[edge[0]][edge[1]] = true
-    matrix[edge[1]][edge[0]] = true
-}
-
-// n번 유저의 케빈 베이컨의 수를 반환하는 bfs
-func bfs(n: Int) -> Int {
-    var queue = Queue<(Int, Int)>() //👉 연결된 유저와 거리를 함께 저장
-    var visited = Array(repeating: false, count: N + 1)
-    var cnt = 0 //👉 거리를 모두 더해서 케빈 베이컨의 수를 구하는 변수
-    queue.push((n, 0))
-    visited[n] = true
-
-    while !queue.isEmpty {
-        let now = queue.pop()
-        cnt += now.1 //👉 거리를 cnt에 더하기
-        for i in 1..<(N+1) {
-            if matrix[now.0][i] && !visited[i] { //👉 now에서 i까지 연결된 간선이 있고 i를 아직 방문하지 않았다면
-                queue.push((i, now.1 + 1))
+        for i in 1..<n+1 {
+            if graph[now.0][i] && !visited[i] {
                 visited[i] = true
+                queue.append((i, now.1 + 1))
             }
         }
+
+        index += 1
     }
+    
     return cnt
 }
 
-// n번 유저의 케빈 베이컨의 수를 저장하는 배열
-var result = Array(repeating: Int.max, count: N + 1)
+var result = Array(repeating: Int.max, count: n+1)
 
-// bfs로 구한 케빈 베이컨의 수를 저장
-for i in 1..<(N+1) {
-    result[i] = bfs(n: i)
+for i in 1..<n+1 {
+    result[i] = bfs(i)
 }
 
-// 가장 작은 케빈 베이컨의 수를 가지는 가장 빠른 index를 반환 (여러명일 경우 가장 빠른 수를 반환하므로)
 print(result.firstIndex(of: result.min()!)!)
