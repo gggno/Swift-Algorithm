8f6bc659f9424d79c1314f3c20e7d69106a2a384
정근호 <geunhojeong.jgh@gmail.com>
2023-09-08 14:42:01 +0900

    [Silver I] Title: 단지번호붙이기, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
@@ -3,45 +3,43 @@ import Foundation
 let n = Int(readLine()!)!
 var graph: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: n)
 
-let dy = [1, -1, 0, 0]
-let dx = [0, 0, 1, -1]
+let dx = [1, -1, 0, 0]
+let dy = [0, 0, 1, -1]
 
-var apartCnt = 0
-var numCnt: [Int] = []
+var allCnt = 0
+var cntArr: [Int] = []
 
 for i in 0..<n {
-    let num = readLine()!.map{Int(String($0))!}
-    graph[i] = num
+    graph[i] = readLine()!.map{Int(String($0))!}
 }
 
 for y in 0..<n {
     for x in 0..<n {
         
         if graph[y][x] == 1 {
-            apartCnt += 1
-            numCnt.append(0)
+            allCnt += 1
+            cntArr.append(0)
             dfs(y, x)
         }
     }
 }
 
 func dfs(_ y: Int, _ x: Int) {
-    numCnt[apartCnt-1] += 1
+    cntArr[allCnt-1] += 1
     graph[y][x] = 0
     
     for i in 0..<4 {
-        let nextY = y + dy[i]
-        let nextX = x + dx[i]
+        let nx = x + dx[i]
+        let ny = y + dy[i]
         
-        if nextY >= 0 && nextX >= 0 && nextY < n && nextX < n {
-            if graph[nextY][nextX] == 1 {
-                dfs(nextY, nextX)
+        if nx >= 0 && nx < n && ny >= 0 && ny < n {
+            if graph[ny][nx] == 1 {
+                dfs(ny, nx)
             }
         }
     }
 }
-
-print(apartCnt)
-for i in numCnt.sorted() {
+print(allCnt)
+for i in cntArr.sorted() {
     print(i)
 }
