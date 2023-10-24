68766f5625ee9e9cb5a538c1914b27d192663a41
정근호 <geunhojeong.jgh@gmail.com>
2023-10-24 15:16:03 +0900

    [level 2] Title: 모음 사전, Time: 1.69 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ word:String) -> Int {
+    var dictionary = [String]()
+    var aeiou = ["A", "E", "I", "O", "U"]
+    var words = ""
+    var index = 0
+    
+    func dfs() {
+        if words.count == 5 {
+            dictionary.append(words)
+            return
+        }
+
+        dictionary.append(words)
+     	
+        for i in 0..<5 {
+            words += String(aeiou[i])
+            dfs()
+            words.removeLast()
+        }
+        return
+    }
+    
+    dfs()
+    
+    return dictionary.firstIndex(of: word)!
+}
