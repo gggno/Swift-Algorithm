eabd531bef08d134566e712a06267ad893189d38
정근호 <geunhojeong.jgh@gmail.com>
2023-11-16 15:05:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 608 ms, Memory: 88216 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
@@ -1,36 +1,34 @@
 import Foundation
 
 let input = readLine()!.split(separator: " ").map{Int($0)!}
-
-let n = input[0]
-let m = input[1]
+let n = input[0], m = input[1]
 
 var graph: [[Int]] = Array(repeating: [], count: n+1)
 var visited: [Bool] = Array(repeating: false, count: n+1)
-var cnt = 0
+
+var result = 0
 
 for _ in 0..<m {
-    let num = readLine()!.split(separator: " ").map{Int($0)!}
-    graph[num[0]].append(num[1])
-    graph[num[1]].append(num[0])
+    let nums = readLine()!.split(separator: " ").map{Int($0)!}
+    graph[nums[0]].append(nums[1])
+    graph[nums[1]].append(nums[0])
 }
 
 for i in 1...n {
     if !visited[i] {
-        cnt += 1
         dfs(i)
+        result += 1
     }
 }
 
-func dfs(_ node: Int) {
-    visited[node].toggle()
-    for i in 0..<graph[node].count {
-        let nextNode = graph[node][i]
-        
-        if !visited[nextNode] {
-            dfs(nextNode)
+func dfs(_ v: Int) {
+    visited[v] = true
+    
+    for i in graph[v] {
+        if !visited[i] {
+            dfs(i)
         }
     }
-    
 }
-print(cnt)
+
+print(result)
