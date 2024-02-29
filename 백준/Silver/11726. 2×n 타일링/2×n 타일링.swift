5984a8e1f30a5cd42e20c7fc4d6b38e4186df64e
정근호 <geunhojeong.jgh@gmail.com>
2024-02-29 17:32:21 +0900

    [Silver III] Title: 2×n 타일링, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n+1)
+
+if n == 1 {
+    print(1 % 10007)
+} else if n == 2 {
+    print(2 % 10007)
+} else {
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in 3..<n+1 {
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    }
+    print(dp[n])
+}
