b3f725eae59548f273f50ee3d8183b81665c9d62
정근호 <geunhojeong.jgh@gmail.com>
2024-09-23 12:01:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.33 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -2,18 +2,18 @@ import Foundation
 
 func solution(_ s:String) -> Int {
     
-    var s = s
+    var ss = s
     
-    s = s.replacingOccurrences(of: "zero", with: "0")
-    s = s.replacingOccurrences(of: "one", with: "1")
-    s = s.replacingOccurrences(of: "two", with: "2")
-    s = s.replacingOccurrences(of: "three", with: "3")
-    s = s.replacingOccurrences(of: "four", with: "4")
-    s = s.replacingOccurrences(of: "five", with: "5")
-    s = s.replacingOccurrences(of: "six", with: "6")
-    s = s.replacingOccurrences(of: "seven", with: "7")
-    s = s.replacingOccurrences(of: "eight", with: "8")
-    s = s.replacingOccurrences(of: "nine", with: "9")
+    ss = ss.replacingOccurrences(of: "zero", with: "0")
+    ss = ss.replacingOccurrences(of: "one", with: "1")
+    ss = ss.replacingOccurrences(of: "two", with: "2")
+    ss = ss.replacingOccurrences(of: "three", with: "3")
+    ss = ss.replacingOccurrences(of: "four", with: "4")
+    ss = ss.replacingOccurrences(of: "five", with: "5")
+    ss = ss.replacingOccurrences(of: "six", with: "6")
+    ss = ss.replacingOccurrences(of: "seven", with: "7")
+    ss = ss.replacingOccurrences(of: "eight", with: "8")
+    ss = ss.replacingOccurrences(of: "nine", with: "9")
     
-    return Int(s)!
+    return Int(ss)!
 }
