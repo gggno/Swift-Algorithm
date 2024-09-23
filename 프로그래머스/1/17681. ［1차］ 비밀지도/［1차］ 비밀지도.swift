def0f14a33c7b6e5f5e2bd6dfccdb41b14b156f6
정근호 <geunhojeong.jgh@gmail.com>
2024-09-23 14:22:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -2,49 +2,47 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     
     var str1: [String] = []
     var str2: [String] = []
-    var str3: [String] = []
-    var result: [String] = []
     
-    for i in arr1 {
-        var str = String(i, radix: 2)
-        var zero = ""
-        
-        for j in str.count..<n {
-            zero += "0"
-        }
-        str = zero + str
-        
-        str1.append(str)
-    }
-    
-    for i in arr2 {
-        var str = String(i, radix: 2)
-        var zero = ""
-        
-        for j in str.count..<n {
-            zero += "0"
-        }
-        str = zero + str
-        
-        str2.append(str)
-    }
-    
+    // 2진수로 변환
     for i in 0..<n {
-        let a1 = Array(String(str1[i]))
-        let a2 = Array(String(str2[i]))
+        var s1 = String(arr1[i], radix: 2)
+        var s2 = String(arr2[i], radix: 2)
         
-        var str = ""
-        
-        for j in 0..<n {
-            if a1[j] == "1" || a2[j] == "1" {
-                str += "#"
-            } else {
-                str += " "
+        if s1.count != n {
+            for j in 0..<n-s1.count {
+                s1 = "0" + s1
             }
         }
         
-        result.append(str)
+        if s2.count != n {
+            for j in 0..<n-s2.count {
+                s2 = "0" + s2
+            }
+        }
+        
+        str1.append(s1)
+        str2.append(s2)
     }
+    
+    var result: [String] = []
+    
+    // 2개의 2진수 배열을 합치기
+    for i in 0..<n {
+        var a1 = Array(str1[i])
+        var a2 = Array(str2[i])
+        var temp: String = ""
+        
+        for j in 0..<n {
+            if a1[j] == "1" || a2[j] == "1" {
+                temp += "#"
+            } else {
+                temp += " "
+            }
+        }
+        
+        result.append(temp)
+    }
+    
     
     return result
 }
