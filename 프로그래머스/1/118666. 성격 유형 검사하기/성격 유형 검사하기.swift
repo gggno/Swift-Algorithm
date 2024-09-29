efec06793b742fd91dbd07b85f6470a57393221b
정근호 <geunhojeong.jgh@gmail.com>
2024-09-29 16:28:21 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.97 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
@@ -2,53 +2,22 @@ import Foundation
 
 func solution(_ survey:[String], _ choices:[Int]) -> String {
     
+    var dic: [Character: Int] = ["R":0, "T":0, "C":0, "F":0, "J":0, "M":0, "A":0, "N":0]
     var result = ""
-    var dic: [String: Int] = ["R":0, "T":0, "C":0, "F":0, "J":0, "M":0, "A":0, "N":0]
     
     for i in 0..<survey.count {
-        let s = Array(survey[i].map{String($0)})
+        if choices[i] < 4 {
+            dic[Array(survey[i])[0]]! += 4 - choices[i]
         
-        switch choices[i] {
-            case 1:
-            dic[s[0]]! += 3
-            case 2:
-            dic[s[0]]! += 2
-            case 3:
-            dic[s[0]]! += 1
-            case 5:
-            dic[s[1]]! += 1
-            case 6:
-            dic[s[1]]! += 2
-            case 7:
-            dic[s[1]]! += 3
-            default:
-            print("end")
+        } else if choices[i] > 4 {
+            dic[Array(survey[i])[1]]! += choices[i] - 4
         }
     }
     
-    if dic["R"]! >= dic["T"]! {
-        result += "R"
-    } else {
-        result += "T"
-    }
-    
-    if dic["C"]! >= dic["F"]! {
-        result += "C"
-    } else {
-        result += "F"
-    }
-    
-    if dic["J"]! >= dic["M"]! {
-        result += "J"
-    } else {
-        result += "M"
-    }
-    
-    if dic["A"]! >= dic["N"]! {
-        result += "A"
-    } else {
-        result += "N"
-    }
+    result += dic["R"]! >= dic["T"]! ? "R" : "T"
+    result += dic["C"]! >= dic["F"]! ? "C" : "F"
+    result += dic["J"]! >= dic["M"]! ? "J" : "M"
+    result += dic["A"]! >= dic["N"]! ? "A" : "N"
     
     return result
 }
