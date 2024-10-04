af1d426333faa8224c3f851fd366053db1276649
정근호 <geunhojeong.jgh@gmail.com>
2024-10-04 15:47:08 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    
+    var prog = progresses
+    var sp = speeds
+    var result: [Int] = []
+    
+    while !prog.isEmpty {
+        for i in 0..<prog.count {
+            prog[i] += sp[i]
+        }
+        
+        if prog.first! >= 100 {
+            var cnt = 0
+            while prog.first! >= 100 {
+                prog.removeFirst()
+                sp.removeFirst()
+                cnt += 1
+                
+                if prog.isEmpty {
+                    break
+                }
+            }
+            
+            result.append(cnt)
+        }
+        
+    }
+    
+    return result
+}
