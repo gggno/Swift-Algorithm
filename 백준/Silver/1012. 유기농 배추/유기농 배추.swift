73e8479d1e86f95322430210a3a50e8a68dc8163
정근호 <geunhojeong.jgh@gmail.com>
2024-10-15 14:38:44 +0900

    [Silver II] Title: 유기농 배추, Time: 16 ms, Memory: 79704 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.swift
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.swift
@@ -1,50 +1,50 @@
 import Foundation
 
 let t = Int(readLine()!)!
-var arr: [[Int]] = []
 
-let dx: [Int] = [0, 0, -1, 1]
-let dy: [Int] = [-1, 1, 0, 0]
+let dx = [1, -1, 0, 0]
+let dy = [0, 0, 1, -1]
 
-var m = 0
-var n = 0
-var k = 0
+var result: [Int] = Array(repeating: 0, count: t)
 
-for _ in 0..<t {
-    let num = readLine()!.split(separator: " ").map{Int($0)!}
-    m = num[0]
-    n = num[1]
-    k = num[2]
-    arr = Array(repeating: Array(repeating: 7, count: m), count: n)
-    var result = 0
+for i in 0..<t {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    let m = input[0], n = input[1], k = input[2]
+    
+    var graph: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: n)
     
     for _ in 0..<k {
-        let coor = readLine()!.split(separator: " ").map{Int($0)!}
-        arr[coor[1]][coor[0]] = 1
+        let nums = readLine()!.split(separator: " ").map{Int($0)!}
+        graph[nums[1]][nums[0]] = 1
     }
     
     for y in 0..<n {
         for x in 0..<m {
-            if arr[y][x] == 1 {
-                dfs(y, x)
-                result += 1
+            if graph[y][x] == 1 {
+                result[i] += 1
+                dfs(y: y, x: x)
             }
         }
     }
-    print(result)
+    
+    func dfs(y: Int, x: Int) {
+        if graph[y][x] == 1 {
+            graph[y][x] = 0
+            
+            for i in 0..<4 {
+                let ny = y + dy[i]
+                let nx = x + dx[i]
+                
+                if 0..<n ~= ny && 0..<m ~= nx {
+                    if graph[ny][nx] == 1 {
+                        dfs(y: ny, x: nx)
+                    }
+                }
+            }
+        }
+    }
 }
 
-func dfs(_ y: Int, _ x: Int) {
-    arr[y][x] = 0
-    
-    for index in 0..<4 {
-        let nextX = x + dx[index]
-        let nextY = y + dy[index]
-        
-        if nextX >= 0 && nextX < m && nextY >= 0 && nextY < n {
-            if arr[nextY][nextX] == 1 {
-                dfs(nextY, nextX)
-            }
-        }
-    }
+for i in result {
+    print(i)
 }
