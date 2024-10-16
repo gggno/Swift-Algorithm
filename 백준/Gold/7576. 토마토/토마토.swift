8f178f1868f55969f3b689979cf8b8be69e46f08
정근호 <geunhojeong.jgh@gmail.com>
2024-10-16 10:43:19 +0900

    [Gold V] Title: 토마토, Time: 256 ms, Memory: 112356 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.swift
+++ 백준/Gold/7576. 토마토/토마토.swift
@@ -6,49 +6,47 @@ let m = input[0], n = input[1]
 let dx = [1, -1, 0, 0]
 let dy = [0, 0, 1, -1]
 
-var graph: [[Int]] = []
-
-for _ in 0..<n {
-    graph.append(readLine()!.split(separator: " ").map{Int($0)!})
-}
-
-var queue: [[Int]] = []
+var graph: [[Int]] = Array(repeating: [], count: n)
 
 for i in 0..<n {
-    for j in 0..<m {
-        if graph[i][j] == 1 {
-            queue.append([i, j])
+    graph[i] = readLine()!.split(separator: " ").map{Int($0)!}
+}
+
+var queue: [(Int, Int)] = []
+var cnt = 1
+
+for y in 0..<n {
+    for x in 0..<m {
+        if graph[y][x] == 1 {
+            queue.append((y, x))
         }
     }
 }
 
 var index = 0
-var result = 1
-
-while index != queue.count {
+while index < queue.count {
     let pop = queue[index]
-    index += 1
     
     for i in 0..<4 {
-        let nx = pop[1] + dx[i]
-        let ny = pop[0] + dy[i]
+        let ny = pop.0 + dy[i]
+        let nx = pop.1 + dx[i]
         
-        if nx >= 0 && nx < m && ny >= 0 && ny < n && graph[ny][nx] == 0 {
-            graph[ny][nx] = graph[pop[0]][pop[1]] + 1
-            queue.append([ny, nx])
+        if 0..<n ~= ny && 0..<m ~= nx && graph[ny][nx] == 0 {
+            graph[ny][nx] = graph[pop.0][pop.1] + 1
+            queue.append((ny, nx))
             
-            if result < graph[pop[0]][pop[1]] + 1 {
-                result = graph[pop[0]][pop[1]] + 1
-            }
+            cnt = max(cnt, graph[pop.0][pop.1] + 1)
         }
     }
+    
+    index += 1
 }
 
 for i in 0..<n {
     if graph[i].contains(0) {
-        result = 0
+        cnt = 0
         break
     }
 }
 
-print(result-1)
+print(cnt-1)
