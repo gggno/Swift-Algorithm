dbf04ffa62f032e2683a72d6c873a0d298281e71
정근호 <geunhojeong.jgh@gmail.com>
2024-10-16 12:03:18 +0900

    [Gold V] Title: 적록색약, Time: 12 ms, Memory: 80060 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.swift
+++ 백준/Gold/10026. 적록색약/적록색약.swift
@@ -2,81 +2,87 @@ import Foundation
 
 let n = Int(readLine()!)!
 
-var graph: [[String]] = []
-var graph2: [[String]] = []
-var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
-var visited2: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
-
 let dx = [1, -1, 0, 0]
 let dy = [0, 0, 1, -1]
 
+var graph: [[String]] = []
+var graph2: [[String]] = []
+
+var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
+var visited2: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
+
 for _ in 0..<n {
-    let input = readLine()!.map{String($0)}
-    graph.append(input)
-    graph2.append(input)
+    let str = readLine()!.map{String($0)}
+    graph.append(str)
+    graph2.append(str)
 }
 
-for i in 0..<n {
-    for j in 0..<n {
-        if graph2[i][j] == "G" {
-            graph2[i][j] = "R"
+for y in 0..<n {
+    for x in 0..<n {
+        if graph2[y][x] == "G" {
+            graph2[y][x] = "R"
         }
     }
 }
 
-var cnt1 = 0
+var cnt = 0
 var cnt2 = 0
-var color1 = ""
+var color = ""
 var color2 = ""
 
+// 적록색맹이 아닌 사람
 for y in 0..<n {
     for x in 0..<n {
         if !visited[y][x] {
             visited[y][x] = true
-            cnt1 += 1
-            color1 = graph[y][x]
-            dfs1(x, y)
+            cnt += 1
+            color = graph[y][x]
+            dfs(y, x)
         }
     }
 }
 
+// 적록색맹인 사람
 for y in 0..<n {
     for x in 0..<n {
         if !visited2[y][x] {
             visited2[y][x] = true
             cnt2 += 1
             color2 = graph2[y][x]
-            dfs2(x, y)
+            dfs2(y, x)
         }
     }
 }
 
-func dfs1(_ x: Int, _ y: Int) {
+// 적록색맹이 아닌 사람
+func dfs(_ y: Int, _ x: Int) {
     for i in 0..<4 {
-        let nx = x + dx[i]
-        let ny = y + dy[i]
+        let ny = dy[i] + y
+        let nx = dx[i] + x
         
-        if nx >= 0 && nx < n && ny >= 0 && ny < n {
-            if !visited[ny][nx] && color1 == graph[ny][nx] {
+        if 0..<n ~= nx && 0..<n ~= ny {
+            if !visited[ny][nx] && graph[ny][nx] == color {
                 visited[ny][nx] = true
-                dfs1(nx, ny)
+                dfs(ny, nx)
             }
         }
     }
 }
 
-func dfs2(_ x: Int, _ y: Int) {
+// 적록색맹인 사람
+func dfs2(_ y: Int, _ x: Int) {
     for i in 0..<4 {
-        let nx = x + dx[i]
-        let ny = y + dy[i]
+        let nx = dx[i] + x
+        let ny = dy[i] + y
         
-        if nx >= 0 && nx < n && ny >= 0 && ny < n {
-            if !visited2[ny][nx] && color2 == graph2[ny][nx] {
+        if 0..<n ~= nx && 0..<n ~= ny {
+            if !visited2[ny][nx] && graph2[ny][nx] == color2 {
                 visited2[ny][nx] = true
-                dfs2(nx, ny)
+                dfs2(ny, nx)
             }
         }
+        
     }
 }
 
-print(cnt1, cnt2)
+print(cnt, cnt2)
