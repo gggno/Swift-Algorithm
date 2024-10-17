45feaaee4ffabc6d777851a500de8fe8e4a766c1
정근호 <geunhojeong.jgh@gmail.com>
2024-10-17 17:34:59 +0900

    [Silver I] Title: 그림, Time: 92 ms, Memory: 128444 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.swift
+++ 백준/Silver/1926. 그림/그림.swift
@@ -1,50 +1,49 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map {Int(String($0))!}
-let n = input[0]
-let m = input[1]
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input[0], m = input[1]
 
 let dx = [1, -1, 0, 0]
 let dy = [0, 0, 1, -1]
-var cnt = 0
-var maxValue = 0
-var tmpMax = 0
+
 var graph: [[Int]] = []
 var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: m), count: n)
 
 for _ in 0..<n {
-    graph.append(readLine()!.split(separator: " ").map {Int(String($0))!})
+    graph.append(readLine()!.split(separator: " ").map{Int($0)!})
 }
+
+var cnt = 0
+var tempCnt = 0
+var maxCnt = 0
 
 for y in 0..<n {
     for x in 0..<m {
         if !visited[y][x] && graph[y][x] == 1 {
             visited[y][x] = true
             cnt += 1
-            tmpMax += 1
-            dfs(x, y)
-            if maxValue < tmpMax {
-                maxValue = tmpMax
-            }
-            tmpMax = 0
+            tempCnt = 0
+            
+            dfs(y, x)
+            maxCnt = max(maxCnt, tempCnt)
         }
     }
 }
 
-func dfs(_ x: Int, _ y: Int) {
+func dfs(_ y: Int, _ x: Int) {
+    tempCnt += 1
     for i in 0..<4 {
-        let nx = x + dx[i]
-        let ny = y + dy[i]
+        let nx = dx[i] + x
+        let ny = dy[i] + y
         
-        if nx >= 0 && ny >= 0 && nx < m && ny < n {
-            if graph[ny][nx] == 1 && !visited[ny][nx] {
+        if 0..<n ~= ny && 0..<m ~= nx {
+            if !visited[ny][nx] && graph[ny][nx] == 1 {
                 visited[ny][nx] = true
-                tmpMax += 1
-                dfs(nx, ny)
+                dfs(ny, nx)
             }
         }
     }
 }
 
 print(cnt)
-print(maxValue)
+print(maxCnt)
