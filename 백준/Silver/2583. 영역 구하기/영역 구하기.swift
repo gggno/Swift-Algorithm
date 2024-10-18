4d8535cbdd6faf5326a4e786ba7f5f1d0b712b63
정근호 <geunhojeong.jgh@gmail.com>
2024-10-18 16:07:25 +0900

    [Silver I] Title: 영역 구하기, Time: 12 ms, Memory: 81268 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.swift
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.swift
@@ -1,62 +1,56 @@
 import Foundation
 
 let input = readLine()!.split(separator: " ").map{Int($0)!}
-
-let m = input[0]
-let n = input[1]
-let k = input[2]
+let m = input[0], n = input[1], k = input[2]
 
 let dx = [1, -1, 0, 0]
 let dy = [0, 0, 1, -1]
 
+var graph: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: m)
+var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: m)
+
+var areaCnt = 0
 var cnt = 0
-var reCnt = 0
 var result: [Int] = []
 
-var graph: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: m)
-var visited: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: m)
-
-for i in 0..<k {
+for _ in 0..<k {
     let nums = readLine()!.split(separator: " ").map{Int($0)!}
-    var reCnt = 0
+    let x1 = nums[0], y1 = nums[1], x2 = nums[2], y2 = nums[3]
     
-    for j in nums[1]..<nums[3] {
-        for k in nums[0]..<nums[2] {
-            graph[j][k] = 1
-            visited[j][k] = 1
+    for y in y1..<y2 {
+        for x in x1..<x2 {
+            graph[y][x] = 1
         }
     }
 }
 
-for j in 0..<m {
-    for k in 0..<n {
-        if visited[j][k] == 0 {
-            cnt += 1
-            dfs(j, k)
-            result.append(reCnt)
-            reCnt = 0
+for y in 0..<m {
+    for x in 0..<n {
+        if !visited[y][x] && graph[y][x] == 0 {
+            visited[y][x] = true
+            areaCnt += 1
+            dfs(y, x)
+            result.append(cnt)
+            cnt = 0
         }
     }
 }
 
 func dfs(_ y: Int, _ x: Int) {
-    if visited[y][x] == 0 {
-        reCnt += 1
-        visited[y][x] = 1
+    cnt += 1
+    
+    for i in 0..<4 {
+        let ny = dy[i] + y
+        let nx = dx[i] + x
         
-        for i in 0..<4 {
-            let nx = x + dx[i]
-            let ny = y + dy[i]
-            
-            if nx >= 0 && ny >= 0 && nx < n && ny < m {
+        if 0..<m ~= ny && 0..<n ~= nx {
+            if !visited[ny][nx] && graph[ny][nx] == 0 {
+                visited[ny][nx] = true
                 dfs(ny, nx)
             }
         }
     }
 }
 
-print(cnt)
-for i in result.sorted() {
-    print(i, terminator: " ")
-}
-print("")
+print(areaCnt)
+print(result.sorted().map{String($0)}.joined(separator: " "))
