f364bf5f4ff17c94547c8a478ace852f27178c3b
정근호 <geunhojeong.jgh@gmail.com>
2024-11-28 10:15:38 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.swift
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.swift
@@ -1,14 +1,14 @@
 func solution(_ n:Int) -> Int {
     
-    var arr: [Int] = [1, 2]
+    var arr = [1, 2]
     
     if n < 3 {
-        return n
+        return n % 1234567
     }
     
     for i in 2..<n {
-        arr.append(arr[i-1] + arr[i-2] % 1234567)    
+        arr.append((arr[i-1] + arr[i-2]) % 1234567)
     }
-            
+    
     return arr.last! % 1234567
 }
