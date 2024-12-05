bf53effb83affc4ec07592c1a110bb0ba2b9f48c
정근호 <geunhojeong.jgh@gmail.com>
2024-12-05 11:19:55 +0900

    [level 2] Title: 할인 행사, Time: 503.12 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.swift
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -1,21 +1,19 @@
 import Foundation
 
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    var answer = 0
-
-    for i in 0..<(discount.count - 9) {
+    var result: Int = 0
+    
+    for i in 0..<discount.count-9 {
         var temp = Array(repeating: 0, count: want.count)
         
-        for j in i..<(i + 10) {
-            if let idx = want.firstIndex(of: discount[j]) {
-                temp[idx] += 1
+        for j in i..<i+10 {
+            if let index = want.firstIndex(of: discount[j]) {
+                temp[index] += 1
             }
         }
-
-        if number == temp {
-            answer += 1
-        }
+        
+        result += temp == number ? 1 : 0
     }
-
-    return answer
+    
+    return result
 }
