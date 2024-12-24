a70930cc3ec839e0c55c37cb64e3e3d37e011f0d
정근호 <geunhojeong.jgh@gmail.com>
2024-12-24 10:29:03 +0900

    [Silver II] Title: 섬의 개수, Time: 12 ms, Memory: 79868 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.swift
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.swift
@@ -1,50 +1,46 @@
 import Foundation
 
-let dx = [1, -1, 0, 0, -1, 1, -1, 1] // 상하좌우, 좌상우상좌하우하
+let dx = [1, -1, 0, 0, -1, 1, -1, 1]
 let dy = [0, 0, 1, -1, 1, 1, -1, -1]
 
 while true {
     let input = readLine()!.split(separator: " ").map{Int($0)!}
+    let w = input[0], h = input[1]
     
-    let w = input[0]
-    let h = input[1]
+    if w == 0 && h == 0 { break }
     
-    if w == 0 && h == 0 {
-        break
-    }
-    
-    var graph: [[Int]] = Array(repeating: [], count: h)
+    var graph: [[Int]] = []
     var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: w), count: h)
-    var count = 0
     
-    for i in 0..<h {
-        let num = readLine()!.split(separator: " ").map{Int($0)!}
-        graph[i] = num
+    var cnt = 0
+    
+    for _ in 0..<h {
+        graph.append(readLine()!.split(separator: " ").map{Int($0)!})
     }
-  
+    
     for y in 0..<h {
         for x in 0..<w {
             if !visited[y][x] && graph[y][x] == 1 {
-                visited[y][x].toggle()
-                dfs(x, y)
-                count += 1
+                visited[y][x] = true
+                cnt += 1
+                dfs(y, x)
             }
         }
     }
     
-    func dfs(_ x: Int, _ y: Int) {
-        for k in 0..<8 {
-            let ny = dy[k] + y
-            let nx = dx[k] + x
+    func dfs(_ y: Int, _ x: Int) {
+        for i in 0..<8 {
+            let ny = y + dy[i]
+            let nx = x + dx[i]
             
-            if ny >= 0 && ny < h && nx >= 0 && nx < w {
+            if 0..<w ~= nx && 0..<h ~= ny {
                 if !visited[ny][nx] && graph[ny][nx] == 1 {
-                    visited[ny][nx].toggle()
-                    dfs(nx, ny)
+                    visited[ny][nx] = true
+                    dfs(ny, nx)
                 }
             }
         }
     }
     
-    print(count)
+    print(cnt)
 }
