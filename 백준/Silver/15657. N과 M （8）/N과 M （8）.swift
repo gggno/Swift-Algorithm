685b3f953b96666eb7c3fdd6f3bb20c87780fa6c
정근호 <geunhojeong.jgh@gmail.com>
2025-01-06 10:23:45 +0900

    [Silver III] Title: N과 M (8), Time: 32 ms, Memory: 80020 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.swift
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.swift
@@ -2,25 +2,26 @@ import Foundation
 
 let input = readLine()!.split(separator: " ").map{Int($0)!}
 let n = input[0], m = input[1]
-let arr = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
+var arr: [Int] = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
 
-var stack: [Int] = []
+var stack: [String] = []
 var answer = ""
 
 func dfs() {
     if stack.count == m {
-        answer += stack.map{String($0)}.joined(separator: " ")
+        answer += stack.joined(separator: " ")
         answer += "\n"
         return
     }
     
     for i in arr {
-        if stack.filter{$0 > i}.count == 0 {
-            stack.append(i)
+        if stack.filter({Int($0)! > i}).count == 0 {
+            stack.append(String(i))
             dfs()
             stack.removeLast()
         }
     }
+    
 }
 
 dfs()
