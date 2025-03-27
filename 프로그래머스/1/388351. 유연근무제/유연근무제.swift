977059871acda70af00bceae146aa27d7146374d
정근호 <geunhojeong.jgh@gmail.com>
2025-03-27 12:21:27 +0900

    [level 1] Title: 유연근무제, Time: 2.63 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.swift
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.swift
@@ -4,29 +4,38 @@ func solution(_ schedules:[Int], _ timelogs:[[Int]], _ startday:Int) -> Int {
     var result = 0
     
     for i in 0..<schedules.count {
-        var goal = schedules[i] + 10
+        var arr: [[Int]] = []
+        var day = startday
         
+        var goal = schedules[i] + 10
+
         if goal % 100 >= 60 {
             goal += 100
             goal -= 60
         }
         
+        for j in 0..<7 {
+            let time = timelogs[i][j]
+            let row = [time, day % 7 == 0 ? 7 : day % 7]
+            arr.append(row)
+            day += 1
+        }
+        
         var state = true
         
-        for j in 0..<7 {
-            let ref = j + startday
+        for k in 0..<arr.count {
+            if arr[k][1] >= 6 { // 주말은 패스
+                continue
+            }
             
-            if ref % 7 == 0 || ref % 7 == 6 { continue }
-            
-            if goal < timelogs[i][j] {
+            // +10보다 큰 경우
+            if goal < arr[k][0] {
                 state = false
                 break
             }
         }
         
-        if state {
-            result += 1
-        }
+        if state { result += 1 }
     }
     
     return result
