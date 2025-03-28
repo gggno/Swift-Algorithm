3677b5f526c8b12839f1e7f6012674c49c56f85f
정근호 <geunhojeong.jgh@gmail.com>
2025-03-28 11:51:28 +0900

    [Silver III] Title: 바이러스, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.swift
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -1,31 +1,28 @@
 import Foundation
 
 let n = Int(readLine()!)!
-let m = Int(readLine()!)!
 
-var graph: [[Int]] = Array(repeating: [], count: n+1)
-var visited: [Bool] = Array(repeating: false, count: n+1)
-
+var graph: [[Int]] = Array(repeating: [], count: n)
+var visited: [Bool] = Array(repeating: false, count: n)
 var cnt = 0
 
-for _ in 0..<m {
-    let nums = readLine()!.split(separator: " ").map{Int($0)!}
+for _ in 0..<Int(readLine()!)! {
+    let nums = readLine()!.split(separator: " ").map{Int($0)! - 1}
     
     graph[nums[0]].append(nums[1])
     graph[nums[1]].append(nums[0])
 }
 
 func dfs(_ num: Int) {
-    if !visited[num] {
-        visited[num] = true
-        cnt += 1
-        
-        for i in graph[num] {
+    for i in graph[num] {
+        if !visited[i] {
+            cnt += 1
+            visited[i] = true
             dfs(i)
         }
     }
 }
 
-dfs(1)
-
-print(cnt-1)
+visited[0] = true
+dfs(0)
+print(cnt)
