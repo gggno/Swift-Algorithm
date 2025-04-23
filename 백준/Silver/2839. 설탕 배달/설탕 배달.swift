e52d85c9f156b21065c5746b994e645debc229bd
정근호 <geunhojeong.jgh@gmail.com>
2025-04-23 10:59:34 +0900

    [Silver IV] Title: 설탕 배달, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.swift
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 var n = Int(readLine()!)!
-var cnt = 0
+var result = 0
 
 while n >= 3 {
     if n % 5 == 0 {
         n -= 5
-        cnt += 1
     } else {
         n -= 3
-        cnt += 1
     }
+    
+    result += 1
 }
 
-print(n == 0 ? cnt : -1)
+print(n == 0 ? result : -1)
