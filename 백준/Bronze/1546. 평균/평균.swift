f2518518ad98cc7e0d38ec1c80fbb118549729e5
정근호 <geunhojeong.jgh@gmail.com>
2025-04-23 10:45:17 +0900

    [Bronze I] Title: 평균, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.swift
+++ 백준/Bronze/1546. 평균/평균.swift
@@ -1,14 +1,13 @@
 import Foundation
 
-let input = Int(readLine()!)!
-
-let score = readLine()!.split(separator: " ").map {Int($0)!}
-
-let m = score.max()!
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int($0)!}
+let m = arr.max()!
 
 var num: Double = 0
 
-for i in 0..<input {
-    num += (Double(score[i]) / Double(m)) * 100
+for i in arr {
+    num += (Double(i) / Double(m)) * 100
 }
-print(num / Double(input))
+
+print(num / Double(n))
