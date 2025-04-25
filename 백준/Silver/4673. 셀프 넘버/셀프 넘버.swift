3540548b00cc8c7785eef78f62115b0ea86cd186
정근호 <geunhojeong.jgh@gmail.com>
2025-04-25 10:44:58 +0900

    [Silver V] Title: 셀프 넘버, Time: 12 ms, Memory: 79776 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.swift
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.swift
@@ -4,18 +4,18 @@ var cnt = 1
 var arr: Set<Int> = []
 
 while cnt <= 10000 {
-    
     let s = String(cnt)
     
-    var sum = 0
+    var sum = Int(s)!
+    
     for c in s {
         sum += Int(String(c))!
     }
-    sum += Int(s)!
     
     arr.insert(sum)
     cnt += 1
 }
+
 cnt = 1
 while cnt <= 10000 {
     if !arr.contains(cnt) {
