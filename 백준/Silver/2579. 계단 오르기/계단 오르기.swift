e8fbf677e72e99eca853719516b3706c927e29c3
정근호 <geunhojeong.jgh@gmail.com>
2025-04-29 10:45:47 +0900

    [Silver III] Title: 계단 오르기, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.swift
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.swift
@@ -1,26 +1,25 @@
 import Foundation
 
 let n = Int(readLine()!)!
-var stairs: [Int] = [0]
+var arr = [0]
 
 for _ in 0..<n {
-    stairs.append(Int(readLine()!)!)
+    arr.append(Int(readLine()!)!)
 }
 
-var cache = [Int](repeating: 0, count: n + 1)
+var dp = [Int](repeating: 0, count: n+1)
 
 if n == 1 {
-    print(stairs[1])
+    print(arr[1])
 } else if n == 2 {
-    print(stairs[1] + stairs[2])
-    
+    print(arr[1] + arr[2])
 } else {
-    cache[1] = stairs[1]
-    cache[2] = stairs[1] + stairs[2]
+    dp[1] = arr[1]
+    dp[2] = dp[1] + arr[2]
     
     for i in 3...n {
-        cache[i] = max(cache[i-3] + stairs[i-1], cache[i-2]) + stairs[i]
+        dp[i] = max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i]
     }
     
-    print(cache.last!)
+    print(dp.last!)
 }
